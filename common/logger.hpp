67d03a6ebdb1a93a4bd9e54a6064c954e3349f04
huwei <coderhw@163.com>
2026-03-29 10:25:47 +0800

    增加es实例以及二次封装api

--- common/logger.hpp
+++ common/logger.hpp
@@ -20,12 +20,12 @@ void init_logger(bool mode, const std::string& filename, int32_t level)
         g_logger->set_level((spdlog::level::level_enum)level);
         g_logger->flush_on((spdlog::level::level_enum)level);
     }
-    // g_logger->set_pattern("[%n][%H:%M:%S][%t][%-8l]%v"); // 可加可不加  
+    // g_logger->set_pattern("[%n][%H:%M:%S][%t][%-8l]%v"); // 可加可不加
 }
 
-#define LOG_TRACE(format, ...) g_logger->trace(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__) 
-#define LOG_DEBUG(format, ...) g_logger->debug(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__) 
-#define LOG_INFO(format, ...) g_logger->info(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__) 
+#define LOG_TRACE(format, ...) g_logger->trace(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) g_logger->debug(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) g_logger->info(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
 #define LOG_WARN(format, ...) g_logger->warn(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) g_logger->error(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)  
-#define LOG_FATAL(format, ...) g_logger->critical(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__) 
+#define LOG_ERROR(format, ...) g_logger->error(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_FATAL(format, ...) g_logger->critical(std::string("[{}:{}]")+format, __FILE__, __LINE__, ##__VA_ARGS__)
--- json/main.cc
+++ json/main.cc
@@ -1,1 +1,37 @@
+#include <json/json.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <sstream>
 
+bool serialize(const Json::Value& val,std::string &dst)
+{
+    Json::StreamWriterBuilder swb;
+    std::unique_ptr<Json::StreamWriter> sw(swb.newStreamWriter());
+    std::stringstream ss;
+    int ret = sw->write(val,&ss);
+    if(ret != 0)
+    {
+        std::cout<<"Json 反序列失败"<<std::endl;
+        return false;
+    }
+    dst = ss.str();
+    return true;
+}
+int main()
+{
+    char name[] = "shuawei";
+    int age = 11;
+    float score[3] = {22,89.5,100};
+    Json::Value stu;
+    stu["姓名"] = name;
+    stu["年龄"] = age;
+    stu["成绩"].append(score[0]);
+    stu["成绩"].append(score[1]);
+    stu["成绩"].append(score[2]);
+    std::string stu_str;
+    bool ret = serialize(stu,stu_str);
+    std::cout<<stu_str<<std::endl;
+    if(ret == false) return -1;
+
+}
